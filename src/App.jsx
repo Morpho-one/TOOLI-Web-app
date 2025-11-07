77bdbbd4363729b31a4a3acd80dfc844efff3c83
Asad <a.ahmed50622@gmail.com>
2025-11-07 10:46:15 +0500

    Half of Sign up Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from 'react'
 import Login from './components/pages/Login/Login'
+import Signup from './components/pages/Signup/Signup'
 
 import './App.css'
 
 
 function App() {
+
+
+  const [showSignup, setShowSignup] = useState(false)
   
 
   return (
     <>
-      <Login />
+      {showSignup ? (
+
+        <Signup onBackToLogin={() => setShowSignup(false)}></Signup>
+      ):(
+        <Login onSignupClick={() => setShowSignup(true)}></Login>
+      )}
     </>
   )
 }
